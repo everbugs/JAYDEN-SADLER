9e053bb3d816bc5acb17520e4bf310668a6499a7
everbugs <114420349+everbugs@users.noreply.github.com>
2020-10-18 10:08:47 +0000

    fixed readme.

--- Stock/Login.Designer.cs
+++ Stock/Login.Designer.cs
@@ -36,3 +36,16 @@
             this.login_btn = new System.Windows.Forms.Button();
             this.username_lable = new System.Windows.Forms.Label();
             this.password_lable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.clear_btn.Location = new System.Drawing.Point(187, 210);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(112, 40);
+            this.clear_btn.TabIndex = 4;
+            this.clear_btn.Text = "Clear";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
