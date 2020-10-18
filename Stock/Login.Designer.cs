bfefc859170e1c8e75d52ed6c9f86e3eeb400891
everbugs <114420349+everbugs@users.noreply.github.com>
2020-10-18 10:02:12 +0000

    More typos

--- Stock/Login.Designer.cs
+++ Stock/Login.Designer.cs
@@ -20,3 +20,19 @@
             }
             base.Dispose(disposing);
         }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.username_lable = new System.Windows.Forms.Label();
+            this.password_lable = new System.Windows.Forms.Label();
