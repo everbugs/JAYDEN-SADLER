d8f242290e3dc74bba8d5752df6912c29b84ac4a
everbugs <114420349+everbugs@users.noreply.github.com>
2020-10-25 10:53:20 +0000

    update gemfile.lock

--- Stock/Login.cs
+++ Stock/Login.cs
@@ -23,3 +23,18 @@ namespace Stock
         {
             username.Text = "";
             password.Clear();
+            username.Focus();
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            //SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-BJH69LU\ASHISSQL;Initial Catalog=Stock;Integrated Security=True");
+            SqlConnection con = MyConnection.GetConnection();
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT * FROM[dbo].[tbl_login] WHERE username = '" + username.Text + "' AND password = '" + password.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            //CTRL + KD To align
+            if (dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
