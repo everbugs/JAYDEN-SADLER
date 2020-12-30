6f51bb73e9f3e124aa9d18f47b725eeebe5f6fdf
everbugs <114420349+everbugs@users.noreply.github.com>
2020-12-30 09:40:05 +0000

    Auto-generated commit from Glitch

--- Stock/MyConnection.cs
+++ Stock/MyConnection.cs
@@ -12,4 +12,4 @@ namespace Stock
         public static SqlConnection GetConnection()
         {
             SqlConnection con = new SqlConnection();
-            con.ConnectionString = System.Config
+            con.ConnectionString = System.Configuration.Co
