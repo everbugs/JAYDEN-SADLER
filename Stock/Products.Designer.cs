0b6c2fabc649b571d4f4945d1effb798d45e4ec6
everbugs <114420349+everbugs@users.noreply.github.com>
2021-01-17 18:00:59 +0000

    updated test files

---
+++ Stock/Products.Designer.cs
@@ -0,0 +1,17 @@
+
+﻿namespace Stock
+{
+    partial class Products
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
