1099c5ab447b2c3ea5813b874b5866bbf1f53bb2
everbugs <114420349+everbugs@users.noreply.github.com>
2021-01-20 21:36:21 +0000

    Fixes #9

--- Stock/Products.Designer.cs
+++ Stock/Products.Designer.cs
@@ -49,3 +49,17 @@
             this.SuspendLayout();
             // 
             // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Code";
+            // 
+            // pro_code
+            // 
+            this.pro_code.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pro_code.Location = new System.Drawing.Point(51, 91);
+            this.pro_code.Name = "pro_code";
