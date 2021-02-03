9c9f8488921d9c6daee6a98923f3faa3cf240d96
everbugs <114420349+everbugs@users.noreply.github.com>
2021-02-03 22:04:36 +0000

    Fix typo in comments

---
+++ Stock/Products.cs
@@ -0,0 +1,19 @@
+
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Products : Form
+    {
+        public Products()
+        {
+            InitializeComponent();
