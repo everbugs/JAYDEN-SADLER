772e4b0a204c7461476e43be42f781548c43e07b
everbugs <114420349+everbugs@users.noreply.github.com>
2021-04-09 10:03:52 +0000

    Update bbcodeeditor.html

--- Stock/Properties/AssemblyInfo.cs
+++ Stock/Properties/AssemblyInfo.cs
@@ -6,4 +6,5 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stock")]
-[assembly: AssemblyDescription(""
+[assembly: AssemblyDescription("")]
+[assem
