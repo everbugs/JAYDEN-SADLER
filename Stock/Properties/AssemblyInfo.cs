99252393659298307d63f5e6c967b719c75d3410
everbugs <114420349+everbugs@users.noreply.github.com>
2021-07-05 21:23:13 +0000

    7.1.3

--- Stock/Properties/AssemblyInfo.cs
+++ Stock/Properties/AssemblyInfo.cs
@@ -30,4 +30,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as sho
+// by using the '*' as shown below:
+/
