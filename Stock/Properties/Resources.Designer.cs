0234db4e12b39668c69ffcfbecff14a958cdadbb
everbugs <114420349+everbugs@users.noreply.github.com>
2021-08-28 14:54:43 +0000

    Improved styling

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -6,4 +6,6 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 // </auto-generated>
-//--------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+name
