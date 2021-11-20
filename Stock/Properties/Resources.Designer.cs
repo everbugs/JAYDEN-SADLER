a71b689d06f687f21ab9e21bab9eaf0f79d5945d
everbugs <114420349+everbugs@users.noreply.github.com>
2021-11-20 06:16:46 +0000

    Fix make clean

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -21,4 +21,5 @@ namespace Stock.Properties
     // with the /str option, or rebuild your VS project.
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-    [global::System.Runtime.CompilerServices.CompilerGenerate
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    
