7ad868c02a366e5b78d3d88fb3ff00f4e5b458c4
everbugs <114420349+everbugs@users.noreply.github.com>
2021-11-20 06:51:14 +0000

    Bump up versionCode.

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -22,4 +22,4 @@ namespace Stock.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    
+    internal class Reso
