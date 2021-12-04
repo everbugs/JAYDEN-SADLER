70e11e791edfba3a4f1643106f0ab5e7e3909dc2
everbugs <114420349+everbugs@users.noreply.github.com>
2021-12-04 12:15:45 +0000

    Post release version bump.

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -29,4 +29,4 @@ namespace Stock.Properties
 
         private static global::System.Globalization.CultureInfo resourceCulture;
 
-        [global::System.Diagnostics.Code
+        [global::System.Diagnostics.CodeAnalysis.S
