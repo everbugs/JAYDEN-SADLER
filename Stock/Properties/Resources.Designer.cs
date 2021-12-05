e1a313b48c649ba876616aa389a26f38a42a3cf6
everbugs <114420349+everbugs@users.noreply.github.com>
2021-12-05 09:56:22 +0000

    field

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -29,4 +29,4 @@ namespace Stock.Properties
 
         private static global::System.Globalization.CultureInfo resourceCulture;
 
-        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "C
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledP
