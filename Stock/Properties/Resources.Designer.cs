cb451878e1b8b6e9d0703dd637248be6e8fb3887
everbugs <114420349+everbugs@users.noreply.github.com>
2022-01-29 05:43:09 +0000

    Hello, world!

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -44,4 +44,4 @@ namespace Stock.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Re
+                    global::System.Resources
