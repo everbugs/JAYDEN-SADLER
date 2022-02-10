c4884c42886ee1740923c60bac688f88050aa881
everbugs <114420349+everbugs@users.noreply.github.com>
2022-02-10 11:08:01 +0000

    Remove unused member variable

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -44,4 +44,4 @@ namespace Stock.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager
+                    global::System.Resources.ResourceManager temp = new
