eac1454d663d23e57646b306ffa55a6b6d0cc46e
everbugs <114420349+everbugs@users.noreply.github.com>
2022-02-19 10:56:20 +0000

    Update workspace.xml

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -44,4 +44,4 @@ namespace Stock.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Stock.Properties
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Stock.Properties.Resource
