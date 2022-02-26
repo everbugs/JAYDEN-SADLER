63c3257d38b5556b5086e8b73081fb9787d56290
everbugs <114420349+everbugs@users.noreply.github.com>
2022-02-26 08:18:52 +0000

    added templates

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -46,4 +46,5 @@ namespace Stock.Properties
                 {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Stock.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
-           
+                }
+ 
