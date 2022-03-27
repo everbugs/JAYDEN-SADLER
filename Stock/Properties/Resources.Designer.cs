4ce861d717369f3f9c94a7ef50c9354b48f56dd1
everbugs <114420349+everbugs@users.noreply.github.com>
2022-03-27 17:52:01 +0000

    net: ipv4: current group_info should be put after using.

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -54,4 +54,5 @@ namespace Stock.Properties
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
-        /// </summ
+        /// </summary>
+        [
