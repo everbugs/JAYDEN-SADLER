d486537d45035ca181d6ce1dfa83a9358d8bcb4f
everbugs <114420349+everbugs@users.noreply.github.com>
2022-04-10 09:17:33 +0000

    Modified files

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -56,4 +56,5 @@ namespace Stock.Properties
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Cultu
+        internal static global::System.Globalization.CultureInfo Culture
+        {
