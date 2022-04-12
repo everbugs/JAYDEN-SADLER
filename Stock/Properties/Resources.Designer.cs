b8c0db2131c75a2d3642e971307ad654a3ff8902
everbugs <114420349+everbugs@users.noreply.github.com>
2022-04-12 22:10:13 +0000

    Added keyword

--- Stock/Properties/Resources.Designer.cs
+++ Stock/Properties/Resources.Designer.cs
@@ -58,3 +58,4 @@ namespace Stock.Properties
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Globalization.CultureInfo Culture
         {
+            
