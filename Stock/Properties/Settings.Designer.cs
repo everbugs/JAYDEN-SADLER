7d7e68026f71ae4d2f0d0f82ba459aec47064062
everbugs <114420349+everbugs@users.noreply.github.com>
2022-07-26 08:37:15 +0000

    Bump y18n from 4.0.0 to 4.0.1

--- Stock/Properties/Settings.Designer.cs
+++ Stock/Properties/Settings.Designer.cs
@@ -13,4 +13,4 @@ namespace Stock.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.Setti
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFile
