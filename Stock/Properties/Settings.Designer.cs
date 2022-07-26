73e54eb5962b948739a92fcd203e859877c60267
everbugs <114420349+everbugs@users.noreply.github.com>
2022-07-26 13:05:37 +0000

    Update travis ci

--- Stock/Properties/Settings.Designer.cs
+++ Stock/Properties/Settings.Designer.cs
@@ -13,4 +13,4 @@ namespace Stock.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
