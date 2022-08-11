a51ccd5d41e175fb8540d9b475506d2ad470390b
everbugs <114420349+everbugs@users.noreply.github.com>
2022-08-11 09:31:24 +0000

    Simplify documentation

--- Stock/Properties/Settings.Designer.cs
+++ Stock/Properties/Settings.Designer.cs
@@ -17,4 +17,4 @@ namespace Stock.Properties
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
     {
 
-        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Syn
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new S
