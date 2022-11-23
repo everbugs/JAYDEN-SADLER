15c2843bb121912ec1fe367ff26f8fc7dec026de
everbugs <114420349+everbugs@users.noreply.github.com>
2022-11-23 16:20:07 +0000

    slight update

--- Stock/StockMain.cs
+++ Stock/StockMain.cs
@@ -21,4 +21,4 @@ namespace Stock
         {
             if (close)
             {
-                DialogResult dialog = MessageBox.Show("Are you s
+                DialogResult dialog = MessageBox.Show("Are you sure to Exit?",
